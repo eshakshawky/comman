6b718e7a5eef47bf758139e0d50da64ec2a2e484
eshakshawky <34951958+eshakshawky@users.noreply.github.com>
2018-02-15 14:22:04 +0200

    Update winwatch.bat

--- winwatch.bat
+++ winwatch.bat
@@ -5,3 +5,5 @@
   timeout /t 5 > NUL
 goto loop
 #while ($true) {netstat -an | findstr 23560; sleep 5; date}
+#while true; do your_command; sleep5; done
+#while ($true) {netstat -an | findstr 23560; sleep 5; date}
